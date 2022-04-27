fc44b663ebd88d64231732215f6236486c57b33b
PTRL483\Anusha.Lakavath <anusha.lakavath@yesgnome.com>
2022-04-27 22:22:25 +0530

    Different Types of joints Are Tested.

---
+++ Assets/CollisionDone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("enter");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exit");
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Stay");
+    }
+}
